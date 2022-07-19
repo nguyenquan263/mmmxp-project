f03a7d9f07ae2d026298cc86dd170ae537057659
Quan Nguyen <nguyenquan263@gmail.com>
2022-07-18 23:52:39 -0500

    change db

--- src/controllers/SiteController.php
+++ src/controllers/SiteController.php
@@ -1,8 +1,24 @@
 <?php
 include("../services/StartUp.php");
 
-$data = array(
-    "message" => "OK"
-);
+$currentHttpMethod = $_SERVER["REQUEST_METHOD"];
 
-echo json_encode($data);
+echo $currentHttpMethod;
+echo isset($_GET["data"]);
+
+if ($currentHttpMethod == "GET")
+{
+
+} 
+elseif ($currentHttpMethod == "POST")
+{
+
+}
+elseif ($currentHttpMethod == "PUT")
+{
+
+}
+else 
+{
+
+}
